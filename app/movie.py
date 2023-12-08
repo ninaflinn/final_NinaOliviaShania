ce9bb8003c3dc41284f5d4579e4e08efbba3cc43
ninaflinn <142847433+ninaflinn@users.noreply.github.com>
2023-12-08 15:31:03 -0500

    Genre List Function

--- app/movie.py
+++ app/movie.py
@@ -5,37 +5,48 @@ import requests
 
 from app.alpha import API_KEY
 
+def get_genre_list():
+    # TMDb API endpoint for getting the list of movie genres
+    url = 'https://api.themoviedb.org/3/genre/movie/list'
+
+    # Parameters for the API request
+    params = {
+        'api_key': API_KEY,
+    }
+
+    try:
+        # Make the API request
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        # Parse the JSON response
+        data = response.json()
+
+        # Extract and return the list of genres
+        genres = data.get('genres', [])
+        return genres
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error making TMDb API request: {e}")
+        return None
+
+
+
 def fetch_data():
     request_url = f"https://api.themoviedb.org/3/movie/550?api_key={API_KEY}"
+    response = requests.get(request_url)
     parsed_response = json.loads(response.text)
     print(type(parsed_response))
     print(parsed_response.keys())
     #pprint(parsed_response)
 
-    data = parsed_response["data"]
+    #data = parsed_response["genres"]
     return data   
 
-def fetch_movie():
-    request = f"https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key={API_KEY}"
-    parsed_response = json.loads(response.text)
-    print(type(parsed_response))
-    print(parsed_response.keys())
-    #pprint(parsed_response)
-
-    data = parsed_response["data"]
-    return data
-
-
-
-#a = "https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=c195830a96938e95ca34e5e4e3e34126"
-#b = requests.get(a)
-#data = json.loads(b.text)
-#print(data)
-
 if __name__ == "__main__":
-    data = fetch_data
-    movie = fetch_movie
-    print("hello")
+    data = get_genre_list()
+    print(data)
+
 
 
 
---
+++ app/search.py
@@ -0,0 +1,4 @@
+from pandas import read_csv
+
+from app.alpha import API_KEY
+
