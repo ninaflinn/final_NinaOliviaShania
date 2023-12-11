8ef470a8772ec54c9ab7c9effda15adcf6529a39
ninaflinn <142847433+ninaflinn@users.noreply.github.com>
2023-12-11 09:47:35 -0500

    Update routes file (#1)

--- web_app/routes/movie_routes.py
+++ web_app/routes/movie_routes.py
@@ -1,68 +1,68 @@
+from flask import Blueprint, request, render_template
 
-# this is the "web_app/routes/stocks_routes.py" file ...
-
-from flask import Blueprint, request, render_template, redirect, flash
-
-from app.stocks import fetch_stocks_data, format_usd
+from app.movies_by_genre import search_movies_by_genre
 
 movie_routes = Blueprint("movie_routes", __name__)
 
-@movie_routes.route("/movie/form")
-def movies_form():
+@movie_routes.route("/movie/form", methods=["GET"])
+def movie_form():
     print("MOVIE FORM...")
     return render_template("movie_form.html")
 
 @movie_routes.route("/movie/dashboard", methods=["GET", "POST"])
-def movies_dashboard():
-    print("STOCKS DASHBOARD...")
-
+def movie_dashboard():
     if request.method == "POST":
-        # for data sent via POST request, form inputs are in request.form:
-        request_data = dict(request.form)
-        print("FORM DATA:", request_data)
-    else:
-        # for data sent via GET request, url params are in request.args
-        request_data = dict(request.args)
-        print("URL PARAMS:", request_data)
+        # Get form data
+        genre_id = request.form.get("genre_id")
+        start_year = request.form.get("start_year")
+        end_year = request.form.get("end_year")
+        lang = request.form.get("lang")
 
-    symbol = request_data.get("symbol") or "NFLX"
+        print("Received Form Data:", genre_id, start_year, end_year, lang)  # Check form data
 
-    try:
-        df = fetch_stocks_data(symbol=symbol)
-        latest_close_usd = format_usd(df.iloc[0]["adjusted_close"])
-        latest_date = df.iloc[0]["timestamp"]
-        data = df.to_dict("records")
+        # Call function to get movies based on user criteria
+        movies = search_movies_by_genre(genre_id, start_year, end_year, lang)
 
-        flash("Fetched Real-time Market Data!", "success")
-        return render_template("movie_dashboard.html",
-            symbol=symbol,
-            latest_close_usd=latest_close_usd,
-            latest_date=latest_date,
-            data=data
-        )
-    except Exception as err:
-        print('OOPS', err)
+        print("Movies Retrieved:", movies)  # Check movies retrieved
 
-        flash("Market Data Error. Please check your symbol and try again!", "danger")
-        return redirect("/movie/form")
+        if movies:
+            # Display movies if found
+            return render_template("movie_dashboard.html", movies=movies)
+        else:
+            # Handle case when no movies are found
+            return render_template("no_movies_found.html")
 
-#
-# API ROUTES
-#
+    # Direct access to the movie dashboard without form submission
+    print("Direct Access to Movie Dashboard")
+    return render_template("movie_dashboard.html", movies=None)
 
-@movie_routes.route("/api/stocks.json")
-def movie_api():
-    print("STOCKS DATA (API)...")
 
-    # for data supplied via GET request, url params are in request.args:
+
+@movie_routes.route("/api/movies.json")
+def movies_api():
+    print("MOVIES DATA (API)...")
+
+    # For data supplied via GET request, URL params are in request.args:
     url_params = dict(request.args)
     print("URL PARAMS:", url_params)
-    symbol = url_params.get("symbol") or "NFLX"
+    
+    # Get parameters for movie search
+    genre_id = url_params.get("genre_id")
+    start_year = url_params.get("start_year")
+    end_year = url_params.get("end_year")
+    lang = url_params.get("lang")
 
     try:
-        df = fetch_stocks_data(symbol=symbol)
-        data = df.to_dict("records")
-        return {"symbol": symbol, "data": data }
+        # Call function to get movies based on user criteria
+        movies = search_movies_by_genre(genre_id, start_year, end_year, lang)
+
+        if movies:
+            # Convert movie data to dictionary records
+            data = [movie.to_dict() for movie in movies]
+            return {"genre_id": genre_id, "data": data}
+        else:
+            return {"message": "No movies found for the specified criteria."}, 404
+
     except Exception as err:
         print('OOPS', err)
-        return {"message":"Market Data Error. Please try again."}, 404
+        return {"message": "Movie data retrieval error. Please try again."}, 500
