3a41a1ad16ef59618235311536cc306133a20097
ninaflinn <142847433+ninaflinn@users.noreply.github.com>
2023-12-14 19:45:33 -0500

    Update test files

--- test/movies_by_genre_test.py
+++ test/movies_by_genre_test.py
@@ -3,5 +3,7 @@ from app.movies_by_genre import search_movies_by_genre
 def test_searching():
     result = search_movies_by_genre(28)
     assert 'release_date' in list(result[0].keys())
-    #['adult', 'backdrop_path', 'genre_ids', 'id', 'original_language', 'original_title', 'overview', 'popularity', 'poster_path', 'release_date', 'title', 'video', 'vote_average', 'vote_count', 'poster_url']
+    #['adult', 'backdrop_path', 'genre_ids', 'id', 'original_language', 
+    #'original_title', 'overview', 'popularity', 'poster_path', 'release_date', 
+    #'title', 'video', 'vote_average', 'vote_count', 'poster_url']
     
